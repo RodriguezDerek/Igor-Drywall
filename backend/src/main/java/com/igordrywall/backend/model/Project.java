1f0362b3453368accece37a6564a707ecf644284
Derek Rodriguez <derekirodriguez05@gmail.com>
2025-07-17 18:39:05 -0400

    Created entity classes for database

--- backend/src/main/java/com/igordrywall/backend/model/Project.java
+++ backend/src/main/java/com/igordrywall/backend/model/Project.java
@@ -1,4 +1,68 @@
 package com.igordrywall.backend.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "projects")
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "projects", nullable = false)
+    private Integer id;
+
+    @NotEmpty(message = "Project name is required")
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @NotEmpty(message = "Job address is required")
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @NotNull(message = "Date is required")
+    @Column(name = "start_date", nullable = false)
+    private LocalDate startDate;
+
+    @NotEmpty(message = "Status is required")
+    @Column(name = "project_status", nullable = false)
+    private String projectStatus;
+
+    @NotEmpty(message = "Team is required")
+    @Column(name = "team", nullable = false)
+    private String team;
+
+    @Column(name = "client_name")
+    private String clientName = "No client name provided";
+
+    @Column(name = "client_phone_number")
+    private String clientPhoneNumber = "No client phone number provided";
+
+    @Column(name = "contractor_name")
+    private String contractorName = "No contractor name provided";
+
+    @Column(name = "contractor_phone_number")
+    private String contractorPhoneNumber = "No contractor phone number provided";
+
+    @Column(name = "description")
+    private String description = "No description provided";
+
+    @Min(value = 0, message = "Drywall amount can't be negative")
+    @Column(name = "total_drywall")
+    private Integer totalDrywall = 0;
+
+    @Min(value = 0, message = "Cost can't be negative")
+    @Column(name = "total_cost")
+    private Integer totalCost = 0;
 }
--- backend/src/main/java/com/igordrywall/backend/model/User.java
+++ backend/src/main/java/com/igordrywall/backend/model/User.java
@@ -1,4 +1,93 @@
 package com.igordrywall.backend.model;
 
-public class User {
+import com.igordrywall.backend.role.Role;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Integer id;
+
+    @Column(name = "first_name", nullable = false)
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    @Email(message = "Email must be valid")
+    @NotBlank(message = "Email is required and must be unique")
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @Column(name = "date_added")
+    private LocalDate dateAdded;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "is_enabled")
+    private boolean isEnabled = false;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword(){
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
+    }
 }
